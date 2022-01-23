a33d6c5ef71cca141f216d5962b7d3d9b40badc7
WorseluckJohn <84751789+WorseluckJohn@users.noreply.github.com>
2022-01-23 01:59:21 -0500

    Final Commit
    Finished the functionalities, the visuals and all the logistics.

--- BoilermakeIX/Boilermake IX/Assets/GameManager.cs
+++ BoilermakeIX/Boilermake IX/Assets/GameManager.cs
@@ -9,10 +9,13 @@ public class GameManager : MonoBehaviour
     public TractorScript tractorScript;
     public GameObject planter;
     public GameObject tractor;
+    public GameObject rain;
+    public GameObject plants;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         tractor = GameObject.Find("tractor");
         tractorScript = GameObject.Find("tractor").GetComponent<TractorScript>();
         cam1st.enabled = false;
@@ -42,10 +45,25 @@ public class GameManager : MonoBehaviour
 
         }
 
-        if (tractorScript.count == 7)
+        if (tractorScript.count == 6)
         {
-            
+            StartCoroutine(Timer());
+            tractorScript.count++;
         }
+    }
+    IEnumerator Timer()
+    {
+        yield return new 
+        WaitForSecondsRealtime(3);
+        rain.SetActive(true);
+        yield return new WaitForSecondsRealtime(3);
+        rain.SetActive(false);
+        yield return new WaitForSecondsRealtime(1);
+        foreach(Transform plant in plants.transform)
+        {
+            plant.gameObject.transform.localScale = new Vector3(1, Random.Range(.75f, 2), 1);
+        }
+        resetPlanter();
     }
 
     void resetPlanter()
